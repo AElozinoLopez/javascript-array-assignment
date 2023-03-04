9bbb90fc7ef3d1a765529af322c1b719c299b783
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-04 07:50:40 +0100

    Wrote an other logic using the forEach method to check workability

--- array.js
+++ array.js
@@ -5,11 +5,15 @@ const facilitators = [
     {name: "Matt", isInstructor:true}
 ];
 
-function extractKey (arrayobj, name) {
-    for (let key of extractKeysArray) {
-        console.log(key);
-    }
+facilitators.forEach(function(extractKey) {
+    console.log(facilitators[key]);
+})
 
-}
+// function extractKey (arrayobj, name) {
+//     for (let key of extractKeysArray) {
+//         console.log(key);
+//     }
+
+// }
 
 console.log(extractKey());
