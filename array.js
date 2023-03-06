97e50caa3a8c4a56eb0a82a4c6510fc8b29fa55a
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-03-06 07:06:38 +0100

    Updated the logic to return the key of an array

--- array.js
+++ array.js
@@ -1,54 +1,34 @@
 // Exercise 1
-// const facilitators = [
-//     {name: "Elie", isInstructor:true},
-//     {name: "Tim", isInstructor:true},
-//     {name: "Matt", isInstructor:true}
-// ];
-
-// const extractKey = facilitators.forEach(function(extractKey) {
-//     console.log(extractKey);
-// })
-
-// function extractKey (arrayobj, name) {
-//     for (let key of extractKeysArray) {
-//         console.log(key);
-//     }
-
-// }
-
-// console.log(extractKey());
-
-// *****************************************************************************
-const instructors = [
-    {name: "Elia", isInstructor: true},
-    {name: "Tim", isInstructor: true},
-    {name: "Matt", isInstructor: true}
+const facilitators = [
+    {name: "Elie", isInstructor:true},
+    {name: "Tim", isInstructor:true},
+    {name: "Matt", isInstructor:true}
 ];
 
-function extractKey(obj, user) {
-    let newArr = [];
-    obj.forEach((person) => {
-        newArr.push(person[user]);
-    });
-    return newArr;
-} 
-console.log(extractKey(instructors, "name"));
-
-// *******************************************************************************
-const people = [
-    { name: "Elie", isInstructor: true },
-    { name: "Tim", isInstructor: true },
-    { name: "Matt", isInstructor: true }
-];
-
-// Using the map method
-function extractKey(arrObject, name) {
-    let result = arrObject.map((value) => {
-        return value.name
+function extractKey(arrayOfObjects, name) {
+    let instructors = arrayOfObjects.map(function(theInstructors){
+        return theInstructors.name
     })
-    return result;
+
 }
 
-console.log(extractKey(people, 'name'))
+console.log(extractKey(facilitators));
 
-// ******************************************************************************
+// *******************************************************************************
+// const people = [
+//     { name: "Elie", isInstructor: true },
+//     { name: "Tim", isInstructor: true },
+//     { name: "Matt", isInstructor: true }
+// ];
+
+// // Using the map method
+// function extractKey(arrObject, name) {
+//     let result = arrObject.map((value) => {
+//         return value.name
+//     })
+//     return result;
+// }
+
+// console.log(extractKey(people, 'name'))
+
+// // ******************************************************************************
